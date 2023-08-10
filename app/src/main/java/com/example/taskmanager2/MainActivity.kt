125f3397a8dba06e9b92c3585833f5707b463979
Ahmad <khanyousafzaideveloper@gmail.com>
2023-08-10 06:28:08 +0500

    done with UI specifications and string resources

--- app/src/main/java/com/example/taskmanager2/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager2/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager2.ui.theme.TaskManager2Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,14 +42,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskManagerApp(){
-    TaskManagerInfo(image = painterResource(id = R.drawable.ic_task_completed), note1 = "All tasks completed", note2 ="Nice work!" )
+    TaskManagerInfo(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        note1 = stringResource(R.string.all_tasks_completed),
+        note2 = stringResource(R.string.nice_work)
+    )
 }
 @Composable
 fun TaskManagerInfo(image: Painter, note1: String, note2: String) {
-Column {
+Column(modifier = Modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
     Image(painter = image, contentDescription = "logo" )
-    Text(text = note1)
-    Text(text = note2)
+    Text(text = note1, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 24.dp, bottom = 8.dp))
+    Text(text = note2, fontSize = 16.sp)
 }
 
 }
